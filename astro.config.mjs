9da9ff6ae73d5e3e57564b337b9eca9f4785395e
borajin <borajin.soft@gmail.com>
2024-03-22 06:51:11 +0900

    🚀 deploy: netlify 배포 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import sitemap from "@astrojs/sitemap";
 
 import svelte from "@astrojs/svelte";
 
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+
+// https://www.netlify.com/blog/how-to-deploy-astro/
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), svelte()],
+  output: "server",
+  adapter: netlify(),
 });
