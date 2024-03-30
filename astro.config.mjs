24535dcef68e140c4e11f6020f21c73028302fc8
borajin <borajin.soft@gmail.com>
2024-03-30 19:30:30 +0900

    🩹 chor: 문서 사진 이미지 용량 줄이기 / 구글 애널리틱스 태그 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,23 @@ import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
+import partytown from "@astrojs/partytown";
+
 // https://docs.astro.build/en/guides/routing/#modifying-the-slug-example-for-ssr
 // https://www.netlify.com/blog/how-to-deploy-astro/
 // https://astro.build/config
 export default defineConfig({
   site: "https://article.womynarchive.com",
-  integrations: [mdx(), sitemap(), svelte()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    svelte(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: netlify(),
 });
