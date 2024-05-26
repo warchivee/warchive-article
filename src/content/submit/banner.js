999b28f1c569119ffbd5683198ee9963dcbcc98c
Kim Jaea <rkddnjs58@gmail.com>
2024-05-26 23:16:01 +0900

    ✨ feat: 감상공유폼 생성

---
+++ src/content/submit/banner.js
@@ -0,0 +1,8 @@
+export const bannerData = {
+  title: "방금 떠나온 세계",
+  creator: "저자 김초엽",
+  image: "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791160406504.jpg",
+  startDate: "2024-07-26",
+  endDate: "2024-07-30",
+  postDate: "2024-08-01",
+};
