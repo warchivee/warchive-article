ccd2d4cb0ec09b37b485e9258790cd6b92bd6e89
KimJaea <rkddnjs58@gmail.com>
2024-11-16 12:11:15 +0900

    🩹 fix: iphone에서 footer 잘림 현상 해결

--- src/components/RiU/RiUFooter.svelte
+++ src/components/RiU/RiUFooter.svelte
@@ -25,7 +25,7 @@
         gap: 10px;
         align-items: center;
         margin-top: 2rem;
-        padding-bottom: calc(1vh + 10vw);
+        padding-bottom: calc(2vh + 8vw);
         transform: translateZ(0);
     }
     footer * {
